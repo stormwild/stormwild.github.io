28793f1e339d35731b79612049b80bf6c7d52a9c
Alexander Torrijos <artorrijos@gmail.com>
2020-03-30 20:53:17 +0800

    Updates

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,10 @@ import html5 from '../img/html5-logo.svg';
 import css from '../img/css3-logo.svg';
 import js from '../img/js-logo.svg';
 import php from '../img/php-logo.svg';
+import csharp from '../img/c-sharp.svg';
+import angular from '../img/angular.svg';
+import reactjs from '../img/React-icon.svg';
+
 
 import analytics from 'universal-ga';
 analytics.initialize('UA-57710872-1');
