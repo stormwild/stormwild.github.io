d394c549d9bf0205e54b950bcc4be21580fed0d5
Alexander R Torrijos <artorrijos@gmail.com>
2023-02-05 18:36:19 +0800

    Removed initial files for a bare project, added integrations

---
+++ astro.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), image()],
+  vite: {
+    ssr: {
+      noExternal: ['modern-normalize']
+    }
+  }
+});
