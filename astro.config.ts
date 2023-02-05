0e04730fca4208c50b5a1dd95a7503c150506540
Alexander R Torrijos <artorrijos@gmail.com>
2023-02-06 00:32:43 +0800

    Updates

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import image from "@astrojs/image";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), image()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    tailwind(),
+  ],
   vite: {
     ssr: {
-      noExternal: ['modern-normalize']
-    }
-  }
-});
+      noExternal: ['modern-normalize'],
+    },
+  },
+  server: { port: 3000 },
+})
