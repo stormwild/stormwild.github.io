71f25bce4905791fb145a122150345af1daf96ce
Alexander R Torrijos <artorrijos@gmail.com>
2023-10-01 15:35:03 +0800

    Upgrade to Astro 3.2

--- astro.config.ts
+++ astro.config.ts
@@ -9,9 +9,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   compressHTML: false,
-  experimental: {
-    assets: true
-  },
   integrations: [AstroPWA({
     /* your pwa options */
     registerType: "autoUpdate"
