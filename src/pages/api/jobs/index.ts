a3643f1f9b104505732e82061725a5a52df498d2
Alexander R Torrijos <artorrijos@gmail.com>
2023-10-01 14:04:20 +0800

    Updates

--- src/pages/api/jobs/index.ts
+++
@@ -1,9 +0,0 @@
-// { params }: { params: unknown }
-export async function GET() {
-  return new Response(JSON.stringify({ message: 'Hello world' }), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-}
