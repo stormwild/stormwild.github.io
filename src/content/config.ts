518531125390cdfe44708cfa342d2d19717eb5c5
Alexander R Torrijos <artorrijos@gmail.com>
2023-10-26 19:16:22 +0800

    Updates

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from 'astro:content'
+
+const skillsCollection = defineCollection('skills', {
+  type: 'data',
+  schema: z.object({
+    skill: z.string(),
+    proficiency: z.number(),
+    label: z.string(),
+    logo: z.string(),
+  }),
+})
+
+export const collections = {
+  skills: skillsCollection,
+}
