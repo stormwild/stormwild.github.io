74f035646def540b7f8de7cb3265ec46c2a5f872
Alexander R Torrijos <artorrijos@gmail.com>
2024-04-07 18:16:08 +0800

    Added github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://stormwild.github.io',
   image: {
     domains: ['astro.build'],
   },
