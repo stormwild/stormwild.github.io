a675cb9b6a6ded3e34802f02f3869887cd2463e0
Alexander R Torrijos <artorrijos@gmail.com>
2024-04-08 15:37:16 +0800

    Added vitest and updated blog

---
+++ src/lib/common/constants.ts
@@ -0,0 +1,5 @@
+export const BLOG_UNDER_CONSTRUCTION =
+  'Blog still under construction. Please excuse the mess.'
+
+export const BLOG_PAGE_ROOT = '/blog/'
+export const BLOG_POST_ROOT = '/blog/post/'
