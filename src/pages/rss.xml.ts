d865ee0de9f6e2d3b094917894906aea51d871ca
Alexander R Torrijos <artorrijos@gmail.com>
2024-04-13 17:49:14 +0800

    Added styling for rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -12,6 +12,7 @@ export const GET: APIRoute = async (context) => {
       new Date(a.data.published).getTime()
   )
   return rss({
+    stylesheet: '/rss.xsl',
     // `<title>` field in output xml
     title: RSS_SITE_NAME,
     // `<description>` field in output xml
