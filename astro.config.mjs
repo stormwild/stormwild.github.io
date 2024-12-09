14483bff71b813448e591f5e45e44c037445feaf
Alexander R Torrijos <artorrijos@gmail.com>
2024-12-09 15:35:11 +0800

    Added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stormwild.github.io',
@@ -13,5 +15,5 @@ export default defineConfig({
       noExternal: ['bootstrap'],
     },
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
 })
